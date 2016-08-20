2f84c323d99ceab2bfa70a31387150608000c89f
MadiBentley <mb.madisonbentley@gmail.com>
2016-08-19 20:59:34 -0700

    Added helvetica & colouring to app

--- script/app.js
+++ script/app.js
@@ -6,24 +6,26 @@ myApp.controller("newChainController", ["$scope", function ($scope){
 			name: 'Flossing',
 			desc: "Following my dentist's advice and flossing daily to avoid tartar and cavities.",
 			days: ["Su", "M", "Tu", "W", "Th", "F", "Sa"],
-			date: "Aug 3"
+			date: "16/08/2016"
 		},{
 			name: "Running",
 			desc: "Go jogging twice a week for 30 minutes. This is a good way to stay in shape.",
 			days: ["M", "W", "F"],
-			date: "aug 5"
+			date: "19/05/2016"
 		},{
 			name: "Wake up before 9AM",
 			desc: "Get my day started early by being awake early. Even on weekends!",
 			days: ["M", "Tu", "W", "Th", "F"],
-			date: "july 8"
+			date: "01/02/2016"
 		}
 	];
 	$scope.addChain= function(){
 		console.log("submit has been pushed");
-		$scope.habits.push({name: $scope.name, desc: $scope.desc});
+		$scope.date = new Date();
+		$scope.habits.push({name: $scope.name, desc: $scope.desc, date: $scope.date});
 		$scope.name ="";
 		$scope.desc ="";	
+		$scope.date ="";
 	};
 }]);
 
